8983d05a49857b5184c82e9951973dd97cfd7824
saraffff <saraf.saiyara25@gmail.com>
2023-11-11 19:58:38 +0600

    first commit

---
+++ oct15.js
@@ -0,0 +1,8 @@
+ function showmark(){
+ var quiz = 14.5;
+var mid = 20.5;
+var final = 44;
+var total = quiz + mid + final ;
+//document.getElementById("total-mark").innerHTML=`Total: ${total}`;
+ }
+//console.log (total); 
